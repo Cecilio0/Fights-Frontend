d2a71326e74296a72fac662c0fe3ea74ec37ea42
Simón González Montoya <99984830+SimonGonMon@users.noreply.github.com>
2024-05-26 17:19:07 -0500

    Improve FightProfile skeleton

--- src/components/FightProfile/FightProfileSkeleton.tsx
+++ src/components/FightProfile/FightProfileSkeleton.tsx
@@ -2,9 +2,8 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function FightSkeleton() {
   return (
-    <div className="flex space-x-6">
+    <div className="flex space-x-6 justify-center align-middle">
       <div className="flex flex-col items-start space-y-6">
-        <Skeleton className="rounded-full w-48 h-48" />
         <Skeleton className="text-3xl font-bold w-full h-5" />
         <Skeleton className="text-xl font-semibold w-full h-5" />
         <Skeleton className="text-sm w-full h-4" />
@@ -13,9 +12,7 @@ export default function FightSkeleton() {
 
       <div className="flex flex-col space-y-6">
         <Skeleton className="h-32 w-[850px] rounded-xl" />
-        <Skeleton className="h-32 w-[850px] rounded-xl" />
-        <Skeleton className="h-32 w-[850px] rounded-xl" />
-        <Skeleton className="h-32 w-[850px] rounded-xl" />
+        <Skeleton className="h-32 h-[400px] w-[850px] rounded-xl" />
       </div>
     </div>
   );
