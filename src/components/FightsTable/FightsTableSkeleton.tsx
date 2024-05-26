7df94fa50a6ebea149af7a3e469f8c6a726ca1d0
Simón González Montoya <99984830+SimonGonMon@users.noreply.github.com>
2024-05-26 17:11:48 -0500

    Improve skeleton

--- src/components/FightsTable/FightsTableSkeleton.tsx
+++ src/components/FightsTable/FightsTableSkeleton.tsx
@@ -5,20 +5,20 @@ export default function FightsTableSkeleton() {
   return (
     <div className="mx-auto max-w-5xl">
       <div className="flex justify-between space-x-4 mb-4">
-        <Skeleton className="h-8 w-[120px]" />
-        <Skeleton className="h-8 w-8" />
-        <Skeleton className="h-8 w-[120px]" />
-        <Skeleton className="h-8 w-8" />
-        <Skeleton className="h-8 w-[120px]" />
+        <Skeleton className="h-8 w-[80px]" />
+        {/* <Skeleton className="h-8 w-8" /> */}
+        <Skeleton className="h-8 w-[80px]" />
+        {/* <Skeleton className="h-8 w-8" /> */}
+        <Skeleton className="h-8 w-[80px]" />
       </div>
       {[...Array(10)].map((_, i) => (
         <div key={i} className="flex justify-between space-x-4 mb-4">
-          <Skeleton className="h-6 w-[120px]" />
-          <Skeleton className="h-6 w-8" />
-          <Skeleton className="h-6 w-[120px]" />
-          <Skeleton className="h-6 w-8" />
-          <Skeleton className="h-6 w-[120px]" />
-          <Skeleton className="h-6 w-[120px]" />
+          <Skeleton className="h-6 w-[80px]" />
+          {/* <Skeleton className="h-6 w-8" /> */}
+          <Skeleton className="h-6 w-[80px]" />
+          {/* <Skeleton className="h-6 w-8" /> */}
+          <Skeleton className="h-6 w-[80px]" />
+          <Skeleton className="h-6 w-[80px]" />
         </div>
       ))}
     </div>
