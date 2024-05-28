8cabc87ea822120e1bf1d1fd942f2648c7147561
Tuardewijuro <99750572+Tuardewijuro@users.noreply.github.com>
2024-05-27 22:24:22 -0500

    nosenada

---
+++ src/resources/DarkLogo.tsx
@@ -0,0 +1,9 @@
+export const DarkLogo = () => (
+    <img
+        alt="Fighting Club Logo"
+        className="max-w-full h-auto"
+        src="/blackdoglogo.png"
+        width={250}
+        height={250}
+    />
+)
---
+++ src/resources/LightLogo.tsx
@@ -0,0 +1,10 @@
+export const LightLogo = () => (
+    <img
+        alt="Fighting Club Logo"
+        className="max-w-full h-auto"
+        src="/blackdoglogo.png"
+        width={250}
+        height={250}
+        style={{ filter: 'invert(1)' }}
+    />
+)
